d6cbd70d0ca9af5564295e26acd03bc62dea5a15
Akasho09 <aakashbwd@gmail.com>
2024-09-08 00:45:58 +0530

    s1

--- MicroP/16bitsum.asm
+++ MicroP/16bitsum.asm
@@ -2,7 +2,7 @@ MVI C,00  ;carry=0
 LHLD 0000H  ; LOAD L&H with 0000 & 0001
 XCHG  ; exchange LH with DE
 LHLD 0002H ;  load again LH
-DAD D ; add LH & CD at H,L
+DAD D ; double add LH & CD at H,L
 JNC jmpp ; jmp if no carry
 INR C ; incremet carry if carry
 jmpp: SHLD 0005H ; store H,L at 0005,0006
