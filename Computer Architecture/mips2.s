6315af359ead53f7414fd93273e5e2ed5f4abc58
Akasho09 <aakashbwd@gmail.com>
2024-09-08 23:24:55 +0530

    s1

--- Computer Architecture/mips2.s
+++ Computer Architecture/mips2.s
@@ -26,16 +26,21 @@ ADDI $s2 , $s2 , 1
 ;for (int  i = 0; i < n; i++) 
 ;     A[i]= A[i+1]
 
+
 main:
-li $t0 ,0
+    lw   $s1, n                # Load n into $s1(to register) ; loads word
+    la   $s0, A                # Load base address of A into $s0  ;loads address
+    li   $t0, 0                # Initialize i to 0
 
-loop :
-bne $t0 , $s1 , loop_end
-addi $t0 , $t0 ,1
-sll $t2 , $t0 ,2
-add $t2 ,$t2, $s0
+loop:
+    bge  $t0, $s1, end_loop    # If i >= n, exit loop
+    sll  $t1, $t0, 2           # $t1 = i * 4 (to index array A, each element is 4 bytes)
+    add  $t2, $s0, $t1         # $t2 = Address of A[i]
+    addi $t0, $t0, 1           # i = i + 1
+    sll $t1 ,$t0 ,2            ; (i+1 )*4
+    add $t1 , $s0 ,$t1         ;t1 is address of A[i+1] , 4(i+1)+A
+    sw   $t5, 0($t2)           # Store A[i+1](register) into A[i] (memory)
+    j    loop                  # Jump back to the start of the loop
 
-lw $t3 , 0($t2) 
-
-loop_end:
-exit:
+end_loop:
+    exit
